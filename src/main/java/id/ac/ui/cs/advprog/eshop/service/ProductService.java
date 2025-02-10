7d15590ea4dcc7424e9d751269bc6a4752225877
Vincent-Davis <leonardvincent62@gmail.com>
2025-02-10 16:08:08 +0700

    change interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,6 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
+    Product create(Product product);
+    List<Product> findAll();
+    Product findById(String id);
+    void update(Product product);
 }
+
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -28,4 +28,32 @@ public class ProductServiceImpl implements ProductService {
         productIterator.forEachRemaining(allProduct::add);
         return allProduct;
     }
+
+    @Override
+    public Product findById(String id) {
+        Iterator<Product> productIterator = productRepository.findAll(); // Dapatkan iterator
+
+        while (productIterator.hasNext()) { // Looping melalui iterator
+            Product product = productIterator.next();
+            if (product.getProductId().equals(id)) {
+                return product; // Jika ketemu, langsung return
+            }
+        }
+
+        return null; // Jika tidak ditemukan, return null
+    }
+
+
+    @Override
+    public void update(Product product) {
+        List<Product> allProducts = new ArrayList<>();
+        productRepository.findAll().forEachRemaining(allProducts::add);
+
+        for (Product p : allProducts) {
+            if (p.getProductId().equals(product.getProductId())) {
+                p.setProductName(product.getProductName());
+                p.setProductQuantity(product.getProductQuantity());
+            }
+        }
+    }
 }
