0700ff09b7a8d7f788308a149d1cbb1b57dcc096
Vincent-Davis <leonardvincent62@gmail.com>
2025-02-19 15:49:00 +0700

    remove unused comments

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -5,7 +5,6 @@ import id.ac.ui.cs.advprog.eshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
