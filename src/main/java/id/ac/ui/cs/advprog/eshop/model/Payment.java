f770f3bf0be4dde13a9902bafc720a2715661f6e
Vincent-Davis <leonardvincent62@gmail.com>
2025-03-06 18:19:20 +0700

    [RED] create skeleton model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class Payment {
+    private String id;
+    private String method;
+    private String status;
+    private Map<String, String> paymentData;
+
+    // Skeleton constructor
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        // Masih kosong
+    }
+
+    // Getter sekadar placeholder
+    public String getId() {
+        return null;
+    }
+
+    public String getMethod() {
+        return null;
+    }
+
+    public String getStatus() {
+        return null;
+    }
+
+    public Map<String, String> getPaymentData() {
+        return null;
+    }
+}
